84d213f0435ce6aa84a96078ce9815f237caf865
©StalingradTeam <70740073+StalingradTeam@users.noreply.github.com>
2021-03-13 21:55:37 +0300

    Add files via upload

---
+++ django_auth/common/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from common.models import UserProfile
+from django.conf import settings
+
+class ProfileCreationForm(forms.Form):
+    
+    age = forms.IntegerField()
+    sex = forms.CharField(max_length=50)
+    city = forms.CharField(max_length=50)
